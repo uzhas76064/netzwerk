b7d46738ee3c758b05bf8d6716b411d7d5cdc57d
Maksim Hvatov <uzhas76064@gmail.com>
2020-05-26 10:06:34 +0500

    Created components of header nav and propfile

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={logo} alt="logo" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://images.unsplash.com/photo-1590350230463-034169746fbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" alt=""/>
+        </div>
+        <div>
+          ava + descr
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile
