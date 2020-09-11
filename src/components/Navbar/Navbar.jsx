e8e23d5e01c2adb14c2d3f116b115d18a4e1b00d
Maksim Hvatov <uzhas76064@gmail.com>
2020-09-11 13:59:38 +0500

    Changed navigation styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,62 +4,43 @@ import {NavLink} from "react-router-dom";
 
 const Navbar = (props) => {
     return (
-        <nav className="nav">
+        <nav className={classes.nav}>
             <ul>
                 <li className={classes.item}>
-                    <svg className="bi bi-person-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                    </svg>
-                    <NavLink to="/profile">Profile</NavLink>
+                    <NavLink to="/profile">
+                        <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+                        <span>Profile</span>
+                    </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-people-fill" fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-                    </svg>
-                    <NavLink to="/users">Users</NavLink>
+                    <NavLink to="/users">
+                        <i className="fa fa-users" aria-hidden="true"></i>
+                        <span>Users</span>
+                    </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <svg className="bi bi-chat-dots-fill" width="1em" height="1em" viewBox="0 0 16 16"
-                         fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                    </svg>
-                    <NavLink to="/messages">Messages</NavLink>
+                    <NavLink to="/messages">
+                        <i className="fa fa-commenting-o" aria-hidden="true"></i>
+                        <span>Messages</span>
+                    </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <svg className="bi bi-card-heading" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                        <path fillRule="evenodd"
-                              d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>
-                        <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1z"/>
-                    </svg>
-                    <NavLink to="/news">News</NavLink>
+                    <NavLink to="/news">
+                        <i className="fa fa-newspaper-o" aria-hidden="true"></i>
+                        <span>News</span>
+                    </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-music-note-list"
-                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z"/>
-                        <path fill-rule="evenodd" d="M12 3v10h-1V3h1z"/>
-                        <path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1V2.82z"/>
-                        <path fill-rule="evenodd"
-                              d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                    <NavLink to="/music">Music</NavLink>
+                    <NavLink to="/music">
+                        <i className="fa fa-music" aria-hidden="true"></i>
+                        <span>Music</span>
+                    </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <svg className="bi bi-gear-wide" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 0 0-9.995 4.998 4.998 0 0 0 0 9.996z"/>
-                    </svg>
-                    <NavLink to="/settings">Settings</NavLink>
+                    <NavLink to="/settings">
+                        <i className="fa fa-cogs" aria-hidden="true"></i>
+                        <span>Settings</span>
+                    </NavLink>
                 </li>
             </ul>
         </nav>
