3bb1a216b06ecfac48652e412598852f878ae83f
Maksim Hvatov <uzhas76064@gmail.com>
2020-10-02 14:24:02 +0500

    Added validation on the message form

--- src/components/Dialogs/MessageForm.jsx
+++ src/components/Dialogs/MessageForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Field, reduxForm} from "redux-form";
+import {maxLength, required} from "../../utils/validators/validators";
+
+const maxLength100 = maxLength(100);
 
 const MessageForm = props => {
     return (
         <form onSubmit={props.handleSubmit}>
-            <Field component='input' name='messageBody' placeholder="Введите сообщение"/>
+            <Field component='input' validate={[required, maxLength100]} name='messageBody' placeholder="Введите сообщение"/>
             <button>Отправить</button>
         </form>
     );
