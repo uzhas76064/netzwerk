fd3e16a95b36f453580cd330a0a6d2bd2386d0e7
Maksim Hvatov <uzhas76064@gmail.com>
2020-12-11 20:32:27 +0500

    Changed styles

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -5,13 +5,13 @@ import {logout} from "../../redux/authReducer";
 
 class HeaderContainer extends React.Component {
     render() {
-        return <Header {...this.props}/>
+        return <Header {...this.props} smallPhoto={this.props.smallPhoto}/>
     }
 }
 
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
-    login: state.auth.login
+    login: state.auth.login,
 })
 
 export default connect(mapStateToProps, {logout})(HeaderContainer)
